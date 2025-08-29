5a89e4813cb4738c616cfaf94c33a91dd8f528de
Akula lokesh <akulalokesh1234@gmail.com>
2025-08-29 11:28:41 +0530

    Add requirements.txt for Render with firebase-admin 6.6

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -10,7 +10,7 @@ service_account = {
     "type": os.getenv("TYPE"),
     "project_id": os.getenv("PROJECT_ID"),
     "private_key_id": os.getenv("PRIVATE_KEY_ID"),
-    "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),  # convert \n into real newlines
+    "private_key": os.getenv("PRIVATE_KEY"),
     "client_email": os.getenv("CLIENT_EMAIL"),
     "client_id": os.getenv("CLIENT_ID"),
     "auth_uri": os.getenv("AUTH_URI"),
