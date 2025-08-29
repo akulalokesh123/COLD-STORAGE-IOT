095e5cfd2a11829e177ff251154f5469e21c999b
Akula lokesh <akulalokesh1234@gmail.com>
2025-08-29 14:53:13 +0530

    Add requirements.txt for lokesh

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -3,42 +3,17 @@ import time
 import random
 import datetime
 import threading
-from dotenv import load_dotenv
+
 import firebase_admin
 from firebase_admin import credentials, db
 from flask import Flask
 
 # -----------------------------
-# Load environment variables
-# -----------------------------
-if os.path.exists(".env"):
-    print("Loading .env from project root...")
-    load_dotenv(".env")
-elif os.path.exists("/etc/secrets/.env"):
-    print("Loading .env from /etc/secrets...")
-    load_dotenv("/etc/secrets/.env")
-else:
-    print("⚠️ No .env file found!")
-
-# -----------------------------
 # Firebase Setup
 # -----------------------------
-firebase_cred = credentials.Certificate({
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
-    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
-})
-
-firebase_admin.initialize_app(firebase_cred, {
-    "databaseURL": os.getenv("FIREBASE_DATABASE_URL")
+cred = credentials.Certificate("/etc/secrets/serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://cold-storage-iot-default-rtdb.firebaseio.com/"
 })
 
 zones_ref = db.reference("zones")
