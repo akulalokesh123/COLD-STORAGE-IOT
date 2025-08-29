60e3711e53742d7513d41cbd14d42b76358eac85
Akula lokesh <akulalokesh1234@gmail.com>
2025-08-29 14:25:50 +0530

    Add requirements.txt for

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -8,20 +8,12 @@ import threading
 import os
 
 # --- Initialize Firebase using environment variables ---
-firebase_cred = credentials.Certificate({
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
-})
-firebase_admin.initialize_app(firebase_cred, {"databaseURL": os.getenv("FIREBASE_DB_URL")})
+# Load the secret file directly
+cred = credentials.Certificate("/etc/secrets/cold-storage-firebase.json")
 
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://cold-storage-iot-default-rtdb.firebaseio.com"
+})
 zones_ref = db.reference("zones")
 
 zones_ref.set({"test": "success"})
